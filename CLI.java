7fa9b0ebc73fc74a72a028f224b929b31522259a
127AEberhardt <127Aeberhardt@frhsd.com>
2025-09-10 08:26:21 -0400

    printing stuff

--- CLI.java
+++ CLI.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
 public class CLI {
+    public static void main(String[] args) {
+        CLI cli = new CLI();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to RAAScanner");
+        wait(2);
+        cli.printUnits();
+    }
+
     //delay
     public static void wait(int x) {
         try {
@@ -10,16 +18,11 @@ public class CLI {
             Thread.currentThread().interrupt();
         }
     }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to RAAScanner");
-        wait(2);
 
-    }
     public void printUnits(){
         String[] units = {"inch", "feet", "yard", "meter", "kilometer", "micrometer", "millimeter", "centimeter", "mile"};
         for (int i = 0; i < units.length; i++){
-            System.out.println(units[i] + "\t");
+            System.out.println(units[i] + "\t" + UnitConverter.Length.units.get(units[i]));
         }
     }
 }
