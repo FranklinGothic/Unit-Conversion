eca58e2d7c20b6d205d1aea09d5b068b91dd0068
127AEberhardt <127Aeberhardt@frhsd.com>
2025-09-10 08:14:26 -0400

    Units in cli

--- cli.java
+++ cli.java
@@ -1,2 +1,9 @@
+import java.util.*;
 public class cli {
+    public void printUnits(){
+        String[] units = {"inch", "feet", "yard", "meter", "kilometer", "micrometer", "millimeter", "centimeter", "mile"};
+        for (int i = 0; i < units.length; i++){
+            System.out.println(units[i] + "\t");
+        }
+    }
 }
