9022998c6729246c214df84e92ccb19986ac56a5
127ABhatt <127abhatt@frhsd.com>
2025-09-10 08:37:24 -0400

    final and intial

--- CLI.java
+++ CLI.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class CLI {
     public static void main(String[] args) {
         CLI cli = new CLI();
+        UnitConverter converter = new UnitConverter();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to RAAScanner");
         wait(2);
         cli.printUnits();
-        System.out.println("Input number");
+        System.out.println("");
+        System.out.println("Input number: ");
         int inputNumber = scanner.nextInt();
-        wait(1);
-        System.out.println("Input number");
+        System.out.println("Input initial unit: ");
+        String initialValue = scanner.next();
+        System.out.println("Input final unit: ");
+        String finalValue = scanner.next();
+
     }
 
     public static void wait(int x) {
@@ -25,7 +30,7 @@ public class CLI {
     public void printUnits(){
         String[] units = {"inch", "feet", "yard", "meter", "kilometer", "micrometer", "millimeter", "centimeter", "mile"};
         for (int i = 0; i < units.length; i++){
-            System.out.println(units[i] + "\t" + UnitConverter.Length.units.get(units[i]));
+            System.out.println(units[i] + "\t");
         }
     }
 }
