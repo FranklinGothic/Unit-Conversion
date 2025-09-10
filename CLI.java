1135ae7706a557b3719834cd17b41cffaf4a2704
127ABhatt <127abhatt@frhsd.com>
2025-09-10 08:25:44 -0400

    added input number and first user input

--- CLI.java
+++ CLI.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class CLI {
     //delay
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to RAAScanner");
+        wait(2);
+        System.out.println("Input number");
+        int inputNumber = scanner.NextInt();
+        wait(1);
+        System.out.println("Input number");
+
+    }
+
     public static void wait(int x) {
         try {
             Thread.sleep(x * 1000);
@@ -10,12 +22,7 @@ public class CLI {
             Thread.currentThread().interrupt();
         }
     }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to RAAScanner");
-        wait(2);
 
-    }
     public void printUnits(){
         String[] units = {"inch", "feet", "yard", "meter", "kilometer", "micrometer", "millimeter", "centimeter", "mile"};
         for (int i = 0; i < units.length; i++){
