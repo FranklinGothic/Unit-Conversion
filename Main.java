0e6ad0c09fbbe4194113c65031288049d75e6a9c
nulledbird <abirdcoder.dev@gmail.com>
2025-09-11 08:59:11 -0400

    Reflection to get method names now works.

--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         CLI cli = new CLI();
         UnitConverter converter = new UnitConverter();
 
         System.out.println("Welcome to RAAScanner\n");
-
         double toConvertNum = cli.initialAmount();
         String initialUnit = cli.initialUnit();
         String convertUnit = cli.convertUnit();
 
-        String toMethod = "to_" + initialUnit;
-        Method conversionMethod = UnitConverter.Length.class.getMethod(toMethod, double.class);
+        Method conversionMethod = UnitConverter.Length.class.getDeclaredMethod(initialUnit, double.class);
+        Method toMethod = UnitConverter.Length.class.getDeclaredMethod("to_" + convertUnit, double.class);
+
+        System.out.println(toConvertNum + " " + UnitConverter.Length.units.get(initialUnit) + " -> " + UnitConverter.Length.units.get(convertUnit));
+        System.out.println(toMethod.invoke(null, conversionMethod.invoke(null, toConvertNum)) + " " + UnitConverter.Length.units.get(convertUnit));
         //Im done for now so we finish reflection tomrw
     }
 }
